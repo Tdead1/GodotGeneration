b81027cb3a6ae3fe755e4934740614772e5839a6
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2019-02-15 16:23:30 +0100

    CHANGE Bugfixing for the tile inspector.

--- Debug/Debugger.gd
+++ Debug/Debugger.gd
@@ -4,9 +4,9 @@ func Find():
 	var xtext = get_node("x").text;
 	var ytext = get_node("y").text;
 	if(xtext.is_valid_integer() && ytext.is_valid_integer()):
-		if(xtext.to_int() < get_parent().MAPSIZE && ytext.to_int() < get_parent().MAPSIZE && xtext.to_int() >= 0 && ytext.to_int() >= 0):
-			get_node("output").text = str(get_parent().map.find(xtext.to_int(), ytext.to_int()).pos);
-			get_node("id").text = "Type: " + str(get_parent().map.find(xtext.to_int(), ytext.to_int()).type);
+		if(xtext.to_int() < get_parent().get_node("Generator").MAPSIZE && ytext.to_int() < get_parent().get_node("Generator").MAPSIZE && xtext.to_int() >= 0 && ytext.to_int() >= 0):
+			get_node("output").text = str(get_parent().get_node("Generator").map.find(xtext.to_int(), ytext.to_int()).pos);
+			get_node("id").text = "Type: " + str(get_parent().get_node("Generator").map.find(xtext.to_int(), ytext.to_int()).type);
 		else:
 			get_node("output").text = "Input is too large or too small.";
 	else:
